b62c258d98c0adf4a4cddfbca0c45427b3a482ea
Mateusz <135542253+Mateusz42@users.noreply.github.com>
2024-02-05 16:45:52 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
   e2e: {
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "https://www.edu.goit.global/account/login",
+    viewportHeight: 1280,
+    viewportWidth: 720,
+    video: true,
   },
 });
